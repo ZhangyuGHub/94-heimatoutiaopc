262524aa28738ceeae7ca45b3130bbab42b3efbc
ZhangyuGHub <1394956959@qq.com>
2020-03-04 15:32:59 +0800

    增加登录界面手动校验功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,7 +8,7 @@
          <img src="../../assets/img/logo_index.png" alt="">
        </div>
        <!-- 表单 绑定model属性  绑定rules属性(表单验证规则) ref 给el-form一个属性-->
-       <el-form :model="loginForm" :rules="loginRules" style="margin-top:20px">
+       <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
          <!-- 表单容器 设置prop属性 prop表示要校验的字段名-->
          <el-form-item prop="tel">
            <!-- 表单域  v-model双向绑定 -->
@@ -27,7 +27,7 @@
          </el-form-item>
          <!-- 按钮 -->
          <el-form-item>
-           <el-button style="width:100%" type="primary">登录</el-button>
+           <el-button @click="login" style="width:100%" type="primary">登录</el-button>
          </el-form-item>
        </el-form>
     </el-card>
@@ -60,6 +60,13 @@ export default {
         }]
 
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate().then(() => {
+        alert('123')
+      })
     }
   }
 }
