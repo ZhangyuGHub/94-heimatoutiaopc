54748899325eb1b0f05762893694c6f783475887
ZhangyuGHub <1394956959@qq.com>
2020-03-06 14:26:58 +0800

    完成左侧导航栏

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="layout-aside">
+    <div class="title">
+        <img src="../../assets/img/logo_admin.png" style="height:35px">
+    </div>
+    <el-menu background-color="#323745" text-color="#adafb5">
+        <el-menu-item >
+            <i class="el-icon-s-home"></i>
+            <span>首页</span>
+        </el-menu-item>
+        <el-submenu>
+           <template slot="title">
+                <i class="el-icon-s-grid"></i>
+            <span>内容管理</span>
+           </template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+        <el-submenu>
+            <templete slot="title">
+                <i class="el-icon-s-grid"></i>
+                <span>粉丝管理</span>
+            </templete>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+        </el-submenu>
+        <el-menu-item >
+            <i class="el-icon-s-grid"></i>
+            <span>账户信息</span>
+        </el-menu-item>
+    </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-aside{
+    width: 230px;
+    height: 100vh;
+    background-color: #2e2f32;
+    .title{
+        text-align: center;
+        padding: 10px
+
+    }
+    .el-menu{
+        border-right: none;
+        }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-      <h1>我是主页</h1>
-  </div>
+  <el-container>
+    <layout-aside></layout-aside>
+  <el-container>
+    <el-header>
+      <h2>这是顶栏</h2>
+    </el-header>
+    <el-main>
+      <h2>这是主要区域</h2>
+    </el-main>
+  </el-container>
+  </el-container>
 </template>
 
 <script>
+import LayoutAside from '@/components/home/layout-aside.vue'
 export default {
-
+  components: {
+    'layout-aside': LayoutAside
+  }
 }
 </script>
 
